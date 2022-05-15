5e75b018ff5c21b55d4483a184783bb13d032963
Enaium <wyy-666@hotmail.com>
2022-05-15 21:09:23 +0800

    Protection

---
+++ src/main/java/cn/enaium/noexpensive/mixin/ProtectionEnchantmentMixin.java
@@ -0,0 +1,26 @@
+package cn.enaium.noexpensive.mixin;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.enchantment.ProtectionEnchantment;
+import net.minecraft.entity.EquipmentSlot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+/**
+ * @author Enaium
+ */
+@Mixin(ProtectionEnchantment.class)
+public class ProtectionEnchantmentMixin extends Enchantment {
+    protected ProtectionEnchantmentMixin(Weight weight, EnchantmentTarget type, EquipmentSlot[] slotTypes) {
+        super(weight, type, slotTypes);
+    }
+
+    /**
+     * @author Enaium
+     */
+    @Overwrite
+    public boolean differs(Enchantment other) {
+        return super.differs(other);
+    }
+}
