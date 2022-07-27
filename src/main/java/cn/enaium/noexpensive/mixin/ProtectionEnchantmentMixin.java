cdfdd3bcb7f9ec034370a37261856d1d91eba820
Enaium <wyy-666@hotmail.com>
2022-07-27 16:07:02 +0800

    reason

--- src/main/java/cn/enaium/noexpensive/mixin/ProtectionEnchantmentMixin.java
+++ src/main/java/cn/enaium/noexpensive/mixin/ProtectionEnchantmentMixin.java
@@ -19,6 +19,7 @@ public class ProtectionEnchantmentMixin extends Enchantment {
 
     /**
      * @author Enaium
+     * @reason no reason
      */
     @Overwrite
     public boolean canAccept(Enchantment other) {
